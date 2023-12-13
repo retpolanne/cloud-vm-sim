e0bad961e4a11bee159c9b648d2e303ad42b028d
Anne Isabelle "Anya" Macedo <retpolanne@posteo.net>
2023-12-13 17:00:50 -0300

    Adds server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::process::Command;
+use std::thread;
+use axum::{
+    routing::{post},
+    http::StatusCode,
+    Router,
+};
 
-fn main() {
-    qemu_spawn();
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+	.route("/spawn_qemu", post(spawn_qemu_request));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn spawn_qemu_request() -> &'static str {
+    thread::spawn(|| qemu_spawn());
+    "QEMU was spawned :)" 
 }
 
 fn qemu_spawn() {
@@ -25,10 +40,13 @@ fn qemu_spawn() {
 	      "-display", "none",
 	      "-device", "virtio-scsi-pci,id=scsi",
 	      "-device", "e1000,netdev=net0",
+	      "-hda", "./bionic-server-cloudimg-amd64.img",
 	      "-netdev", "user,id=net0,hostfwd=tcp::2222-:22"]);
 
-    if let Ok(child) = cmd.spawn() {
-	println!("Spawned qemu with pid {}", child.id());
+    if let Ok(mut child) = cmd.spawn() {
+	println!("Spawned qemu");
+	child.wait_with_output().expect("Linux");
+	println!("Linux started!");
     } else {
 	println!("Couldn't start qemu");
     }
