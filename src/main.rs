afc86b83bd998c759c659542261d410b90a43cb1
Anne Isabelle "Anya" Macedo <retpolanne@posteo.net>
2023-12-13 20:59:26 -0300

    Shared state working

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,51 @@ use std::collections::HashMap;
 use std::thread;
 use std::str;
 use axum::{
-    extract::{Path},
+    extract::{Path, Extension},
     routing::{post, get},
     Router,
 };
+use std::sync::{Arc, RwLock};
+
+struct VMState {
+    vm_map: HashMap<String, String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let var = "abc";
+    let shared_state : Arc<RwLock<VMState>> = Arc::new(RwLock::new(VMState {
+	vm_map: HashMap::new(),
+    }));
     let app = Router::new()
 	.route("/spawn_qemu/:name",
-	       post(
-		   move |path: Path<String>, body: String|
-		   spawn_qemu_request(path, body)))
+	       post(spawn_qemu_request))
 	.route("/:name/user-data",
-	       get(user_data_request));
+	       get(user_data_request))
+	.layer(Extension(shared_state));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn spawn_qemu_request(Path(name): Path<String>, user_data: String) -> &'static str {
+async fn spawn_qemu_request(
+    Extension(state) : Extension<Arc<RwLock<VMState>>>, Path(name): Path<String>, user_data: String
+) -> &'static str {
+    let user_data_clone = user_data.clone();
+    state.write().unwrap().vm_map.insert(name, user_data_clone);
     println!("{}", user_data);
-    thread::spawn(|| qemu_spawn(name));
+    //thread::spawn(|| qemu_spawn(name));
     "QEMU was spawned :)" 
 }
 
-async fn user_data_request(Path(name): Path<String>) -> String {
-    name
+async fn user_data_request(
+    Extension(state) : Extension<Arc<RwLock<VMState>>>, Path(name): Path<String>
+) -> String {
+    let vm_map = &state.read().unwrap().vm_map;
+
+    if let Some(value) = vm_map.get(&name) {
+        value.clone()
+    } else {
+	format!("Couldn't get map for {}", name)
+    }
 }
 
 fn qemu_spawn(name: String) {
