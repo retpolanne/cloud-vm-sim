882db07b310f5b5eb041f0b396a5ba11a56c5683
Anne Isabelle "Anya" Macedo <retpolanne@posteo.net>
2023-12-14 09:43:49 -0300

    Fixes cloud-init

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,36 @@ use axum::{
     routing::{post, get},
     Router,
 };
+use tower_http::trace::TraceLayer;
+use tracing_subscriber;
 use std::sync::{Arc, RwLock};
 
 struct VMState {
     vm_map: HashMap<String, String>,
 }
 
-#[tokio::main]
-async fn main() {
+fn router() -> Router {
     let shared_state : Arc<RwLock<VMState>> = Arc::new(RwLock::new(VMState {
 	vm_map: HashMap::new(),
     }));
-    let app = Router::new()
+    Router::new()
 	.route("/spawn_qemu/:name",
 	       post(spawn_qemu_request))
 	.route("/:name/user-data",
 	       get(user_data_request))
-	.layer(Extension(shared_state));
+	.route("/:name/meta-data",
+	       get(meta_data_request))
+	.layer(TraceLayer::new_for_http())	
+	.layer(Extension(shared_state))
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+	.with_max_level(tracing::Level::DEBUG)
+	.init();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, router()).await.unwrap();
 }
 
 async fn spawn_qemu_request(
@@ -51,12 +62,18 @@ async fn user_data_request(
     }
 }
 
+async fn meta_data_request(
+    Path(name): Path<String>
+) -> String {
+    format!("instance-id: {}", name)
+}
+
 fn qemu_spawn(name: String) {
     let _ = fs::create_dir_all("/tmp/qemu-logs");
     let log_name = format!("/tmp/qemu-logs/{}.log", name.clone());
-    let log = File::create(log_name).expect("failed to open log");
+    let log = File::create(log_name.clone()).expect("failed to open log");
     let err_log_name = format!("/tmp/qemu-logs/{}-stderr.log", name.clone());
-    let err_log = File::create(err_log_name).expect("failed to open log");
+    let err_log = File::create(err_log_name.clone()).expect("failed to open log");
     let mut cmd = if cfg!(target_arch = "aarch64") {
 	Command::new("qemu-system-aarch64")
     } else {
@@ -78,17 +95,16 @@ fn qemu_spawn(name: String) {
 	      "-device", "virtio-scsi-pci,id=scsi",
 	      "-device", "e1000,netdev=net0",
 	      "-hda", "./bionic-server-cloudimg-amd64.img",
-	      "--smbios", format!("type=1,serial=ds='nocloud-net;s=http://10.0.2.2:3000/{}'", name).as_str(),
+	      "--smbios", format!("type=1,serial=ds=nocloud-net;s=http://10.0.2.2:3000/{}/", name).as_str(),
 	      "-netdev", "user,id=net0,hostfwd=tcp::2222-:22"]);
 
     cmd.stdout(log);
     cmd.stderr(err_log);
 
     if let Ok(child) = cmd.spawn() {
-	println!("Spawned qemu");
-	child.wait_with_output().expect("Linux");
-	println!("Linux started!");
+	println!("Spawned qemu - logging to {}", log_name);
+	child.wait_with_output().expect("Error with this qemu machine");
     } else {
-	println!("Couldn't start qemu");
+	println!("Couldn't start qemu - error at {}", err_log_name);
     }
 }
